9a277b8033f94b47c0b7a90ef36e7fe188d890eb
clementfoltran <clfoltra@student.42.fr>
2019-01-28 17:34:56 +0100

    end of day 1

--- src/fdf.c
+++ src/fdf.c
@@ -6,7 +6,7 @@
 /*   By: clfoltra <clfoltra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/28 13:23:14 by clfoltra          #+#    #+#             */
-/*   Updated: 2019/01/28 15:50:57 by cvignal          ###   ########.fr       */
+/*   Updated: 2019/01/28 17:27:07 by clfoltra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,64 @@
 #include "libft.h"
 #include "parser.h"
 #include "mlx.h"
+#include <math.h>
 
-static void	usage(void)
+// static void	usage(void)
+// {
+// 	ft_printf("usage : fdf file1\n");
+// }
+
+static void iso(int *x, int *y, int z)
 {
-	ft_printf("usage : fdf file1\n");
+    int previous_x;
+    int previous_y;
+
+    previous_x = *x;
+    previous_y = *y;
+    *x = (previous_x - previous_y) * cos(0.523599);
+    *y = -z + (previous_x + previous_y) * sin(0.523599);
 }
 
-int main(int ac, char **av)
+static void	draw_segment(int x1, int y1, int x2, int y2)
 {
-	t_list	*points;
-
-    if (ac != 2)
+	int dx;
+	int dy;
+	int e;
+	void *mlx;
+	void *window;
+	
+	mlx = mlx_init();
+	window = mlx_new_window(mlx, WINDOW_X, WINDOW_Y, "FdF");
+	e = x2 - x1;
+	dx = e * 2;
+	dy = (y2 - y1) * 2;
+	while (x1 <= x2)
 	{
-		usage();
-		return (1);
+		mlx_pixel_put(mlx, window, x1, y1, 0xFFFFFF);
+		x1++;
+		if ((e -= dy) <= 0)
+		{
+			y1++;
+			e += dx;
+		}
 	}
-	if (!(points = fdf_parser(av[1])))
-		return (1);
+	mlx_loop(mlx);
+}
+
+int			main(int ac, char **av)
+{
+	// t_list	*points;
+	(void) ac;
+	(void) av;
+
+	draw_segment(250, 250, 500, 500);
+
+    // if (ac != 2)
+	// {
+	// 	usage();
+	// 	return (1);
+	// }
+	// if (!(points = fdf_parser(av[1])))
+	// 	return (1);
     return (0);
 }
