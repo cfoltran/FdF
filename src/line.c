a7e7dbe383024f8890d25f2f092804b76bf30edb
clementfoltran <clfoltra@student.42.fr>
2019-02-22 00:11:25 +0100

    TODO : errors

--- src/line.c
+++ src/line.c
@@ -6,7 +6,7 @@
 /*   By: clfoltra <clfoltra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/11 15:27:33 by clfoltra          #+#    #+#             */
-/*   Updated: 2019/02/21 16:52:28 by clfoltra         ###   ########.fr       */
+/*   Updated: 2019/02/22 00:07:12 by clfoltra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ void	case1(t_env *env, t_pt pt)
 		ft_swapint(&pt.y1, &pt.y2);
 	}
 	a = ((float)(pt.y2 - pt.y1) / (pt.x2 - pt.x1));
-	while (i < pt.dx + 1)
+	while (i <= pt.dx)
 	{
 		if (pt.x1 + i > 0 && pt.x1 + i < env->win_w && (pt.y1 +
 		(int)(i * a)) > 0 && pt.y1 + (int)(i * a) < env->win_h)
@@ -48,7 +48,7 @@ void	case2(t_env *env, t_pt pt)
 		ft_swapint(&pt.x1, &pt.x2);
 	}
 	a = (float)(pt.x2 - pt.x1) / (pt.y2 - pt.y1);
-	while (++i < pt.dy + 1)
+	while (++i <= pt.dy)
 	{
 		if (pt.x1 + (int)(i * a) > 0 && (pt.x1 + (int)(i * a)) < env->win_w && (pt.y1 + i) > 0
 											&& pt.y1 + i < env->win_h)				
